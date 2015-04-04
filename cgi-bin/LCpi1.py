b19ee7be1014eed8dff02624a0dfdc684fd62e9b
drkmsmithjr <drkmsmithjr@gmail.com>
2015-04-04 09:01:17 -0700

    Update LCpi1.py
    chnagee the directories to LConnect

--- cgi-bin/LCpi1.py
+++ cgi-bin/LCpi1.py
@@ -22,8 +22,8 @@ cgitb.enable()
 def printHTTPheader():
     print "Content-type: text/html\n"
 
-statfile = "/home/pi/LawnConnect/status.txt" 
-statfile3 = "/home/pi/LawnConnect/status3.txt" 
+statfile = "/home/pi/LConnect/status.txt" 
+statfile3 = "/home/pi/LConnect/status3.txt" 
 dateformat = "%d-%m-%y: %I:%M %p"   
 
 
